7124d624b7c853c5937d66dc036751efb6132a56
yohagos <yosef.hagos@googlemail.com>
2026-09-20 19:46:17 +0200

    feat: authentification interceptor to add token if it exists

---
+++ angular-workspace/projects/shared-data-access/src/lib/interceptors/auth.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { TokenService } from "../services/token.service";
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const tokenService = inject(TokenService)
+  const token = tokenService.getToken()
+
+  if (!token) {
+    return next(req)
+  }
+
+  return next(req.clone({
+    setHeaders: { Authorization: `Bearer ${token}`}
+  }))
+}
